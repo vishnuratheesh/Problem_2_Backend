d3781f42e0bfc5ee27c35923ffcb79615732503f
Vishnu Ratheesh <vishnu@levitum.com>
2015-06-06 05:26:15 +0530

    Minor fixes.

--- api.py
+++ api.py
@@ -7,13 +7,29 @@ app = Flask(__name__)
 def hello_world():
     return 'Problem_2 Backend APIs'
 
-@app.route('/tickets',methods=['GET'])
+@app.route('/v1/tickets',methods=['GET'])
 def getAllTickets():
-  recs = getAllTicketDetails(g.user_id)
+  recs = getAllTicketDetails()
   if recs:
     return jsonify(data=recs)
   else:
-    return page_not_found('User Not Found')
+    return page_not_found('No Tickets Found')
+
+#--- Error Code 
+@app.errorhandler(404)
+def page_not_found(error):
+  if isinstance(error, basestring):
+    return jsonify(status=error), 404
+  else:
+    return "not found",404
+
+@app.errorhandler(400)
+def missing_parameter(error):
+  print error
+  if isinstance(error, basestring):
+    return jsonify(status=error), 400
+  else:
+    return "invalid request", 400 
 
 
 if __name__ == '__main__':
